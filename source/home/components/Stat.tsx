9fd4e537c955d4538a6614e26c296422be6388b7
João Veiga Moraes <joaoveigamoraes@MacBook-Pro-de-Joao.local>
2022-11-16 20:53:32 -0300

    fix stats

--- source/home/components/Stat.tsx
+++ source/home/components/Stat.tsx
@@ -1,73 +1,88 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
-import { Theme, getThemeByType } from "../../theme"
-import { MAX_STATS } from '../core/constants'
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {Theme, getThemeByType} from '../../theme';
+import {MAX_STATS} from '../core/constants';
 
-type FullAttributeType = 'hp' | 'attack' | 'defense' | 'special-attack' | 'special-defense' | 'speed'
-type ShortAttributeType = 'HP' | 'ATK' | 'DEF' | 'SATK' | 'SDEF' | 'SPD'
-type ShortStatType = [FullAttributeType, ShortAttributeType]
+type FullAttributeType =
+  | 'hp'
+  | 'attack'
+  | 'defense'
+  | 'special-attack'
+  | 'special-defense'
+  | 'speed';
+type ShortAttributeType = 'HP' | 'ATK' | 'DEF' | 'SATK' | 'SDEF' | 'SPD';
+type ShortStatType = [FullAttributeType, ShortAttributeType];
 
 export type StatType = {
-  name: FullAttributeType
-  baseStat: number
-  type: string
-}
+  name: FullAttributeType;
+  baseStat: number;
+  type: string;
+};
 
-
-export default function Stat({ name, baseStat, type }: StatType) {
+export default function Stat({name, baseStat, type}: StatType) {
   const dictionaryStat: ShortStatType[] = [
     ['hp', 'HP'],
     ['attack', 'ATK'],
     ['defense', 'DEF'],
     ['special-attack', 'SATK'],
     ['special-defense', 'SDEF'],
-    ['speed', 'SPD']
-  ]
-  const [, shortName] = dictionaryStat.find(([item,]) => item === name) as ShortStatType
+    ['speed', 'SPD'],
+  ];
+  const [, shortName] = dictionaryStat.find(
+    ([item]) => item === name,
+  ) as ShortStatType;
 
   function percentStat() {
-    return (baseStat / MAX_STATS[shortName] * 100)
+    return (baseStat / MAX_STATS[shortName]) * 100;
   }
 
   function paintStatBar() {
-    return percentStat().toString().concat('%')
+    return percentStat().toString().concat('%');
   }
 
   function opacityStatBar() {
-    return (100 - percentStat()).toString().concat('%')
+    return (100 - percentStat()).toString().concat('%');
   }
 
   return (
     <View style={styles.stat}>
       <View style={styles.statContainer}>
-        <Text style={[styles.statText, { color: getThemeByType(type), }]}>
+        <Text style={[styles.statText, {color: getThemeByType(type)}]}>
           {shortName}
         </Text>
       </View>
       <View style={styles.separator} />
-      <Text style={styles.numberText}>{baseStat > 99 ? baseStat : '0' + baseStat}</Text>
+      <Text style={styles.numberText}>
+        {baseStat > 99 ? baseStat : '0' + baseStat}
+      </Text>
       <View style={styles.fullStatusBar}>
-        <View style={[
-          styles.barWithoutOpacity,
-          {
-            backgroundColor: getThemeByType(type),
-            width: paintStatBar(),
-          }]} />
-        <View style={[
-          styles.barWithOpacity,
-          {
-            backgroundColor: getThemeByType(type).concat(50),
-            width: opacityStatBar(),
-          }]} />
+        <View
+          style={[
+            styles.barWithoutOpacity,
+            {
+              backgroundColor: getThemeByType(type),
+              width: paintStatBar(),
+            },
+          ]}
+        />
+        <View
+          style={[
+            styles.barWithOpacity,
+            {
+              backgroundColor: getThemeByType(type).concat(50),
+              width: opacityStatBar(),
+            },
+          ]}
+        />
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   barWithoutOpacity: {
     borderTopLeftRadius: 50,
-    borderBottomLeftRadius: 50
+    borderBottomLeftRadius: 50,
   },
   barWithOpacity: {
     borderTopRightRadius: 50,
@@ -77,18 +92,19 @@ const styles = StyleSheet.create({
     height: 5,
     alignSelf: 'center',
     flexDirection: 'row',
-    width: '75%'
+    width: '75%',
   },
   numberText: {
-    color: Theme.darkGray
+    color: Theme.darkGray,
   },
   statContainer: {
     flexDirection: 'row',
-    width: '10%',
+    width: '12%',
     justifyContent: 'flex-end',
   },
   statText: {
-    textAlign: 'center'
+    textAlign: 'right',
+    fontWeight: 'bold',
   },
   separator: {
     width: 1,
@@ -99,5 +115,5 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingHorizontal: 20,
     justifyContent: 'space-between',
-  }
-})
+  },
+});
--- source/home/components/Stats.tsx
+++ source/home/components/Stats.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { Stat, StatType } from './'
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Stat, StatType} from './';
 
 type StatsType = {
-  type: string
-  stats: StatType[]
-}
+  type: string;
+  stats: StatType[];
+};
 
-export default function Stats({ type, stats }: StatsType) {
+export default function Stats({type, stats}: StatsType) {
   const renderStats = () =>
-    stats.map(({ name, baseStat }, key) => <Stat {...{ type, key, name, baseStat }} />)
+    stats.map(({name, baseStat}, key) => (
+      <Stat {...{type, key, name, baseStat}} />
+    ));
   return (
     <View style={styles.container}>
-      <View style={styles.statContainer}>
-        {renderStats()}
-      </View>
+      <View style={styles.statContainer}>{renderStats()}</View>
     </View>
-
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -28,5 +27,5 @@ const styles = StyleSheet.create({
   statContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-  }
-})
+  },
+});
